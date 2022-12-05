c855a0fb56b526285e66521c4a8019346baa7f6f
SamuraX <matheusomr1@gmail.com>
2022-12-05 13:05:38 -0300

    build: add full typescript support

---
+++ hardhat.config.ts
@@ -0,0 +1,15 @@
+import "@nomicfoundation/hardhat-toolbox";
+import "tsconfig-paths/register";
+import { HardhatUserConfig } from "hardhat/types";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.17",
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+};
+
+export default config;
